2a0784c82adf9b816e7b601a78f1c85d72db6831
Khushbu712 <khushbuchoudhary2523@gmail.com>
2024-05-19 13:23:33 +0530

    mofified

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import styles from "../styles/Total.module.css";
-import { itemContext } from "../context";
-import { useContext } from "react";
+import styles from "../styles/Navbar.module.css";
+import { useValue } from "../itemCustomContext";
 
 function Navbar() {
-  const  value = useContext(itemContext);
+  const {clear,item,total,toggle} = useValue();
 
   return (
     <div className={styles.container}>
-      <h1>Total : &#x20B9; {value.total}</h1>
-      <h1>Items: {value.item}</h1>
-    </div>
+      <h1>Total : &#x20B9; {total}</h1>
+      <h1>Items: {item}</h1>
+     <div className={styles.buttonsWrapper}>
+     <button className={styles.button} onClick={toggle}>Cart</button>
+     <button className={styles.button} onClick={clear}>Reset</button>
+     </div>
+        </div>
   );
 }
 
